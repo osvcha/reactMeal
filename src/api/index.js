9c903a7a1550274ddf1fe7f1621cd6d993c127d7
Osvaldo Chaparro Bolaños <osvcha@gmail.com>
2021-07-14 21:50:53 +0100

    API Basic config

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import {API_URL} from '../config/api';
+
+const aInstance = axios.create({
+  baseURL: API_URL,
+  headers: {'Content-Type': 'application/json'},
+});
+
+export const getCategories = () => {
+  const url = '/categories.php';
+  return aInstance.get(url);
+};
